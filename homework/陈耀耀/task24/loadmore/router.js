4f466b3f7ccd5caf3c4167dacf69b8e6b2f3ec4a
yasinchen <yasinchan2016@gmail.com>
2017-01-14 02:21:19 +0800

    update task24

---
+++ homework/陈耀耀/task24/loadmore/router.js
@@ -0,0 +1,68 @@
+/**
+ * 使用范例
+ */
+
+
+/**
+ * 发送 GET 请求， 无参数
+ * GET /hello
+ * 返回响应数据
+ */
+app.get('/loading', function(req, res) {
+	var start = req.query.start,
+		len = req.query.len;
+	var data = [];
+
+	for(var i=start; i<start+6 ;i++){
+		data.push(i);
+	}
+	res.send({
+		status: 1,
+		data:data
+	});
+});
+
+
+/**
+ * 发送 GET 请求, 有参数
+ * GET /user/100
+ * query = { name: 'ruoyu', age: 28 }
+ */
+app.get('/user/:uid', function(req, res) {
+	console.log(req.params.uid); //100
+	console.log(req.query.name); // 'ruoyu'
+	res.send({
+		status: 1,
+		errorMsg: "请先注册"
+	});
+});
+
+
+/**
+ * 发送 POST 请求， 有参数
+ * POST /comment
+ * query = { comment: "这是评论内容" }
+ */
+app.post('/comment', function(req, res) {
+	console.log(req.body.comment); // "这是评论内容"
+	res.send({
+		status: 0,
+		data: {
+			cid: 100,
+			comment: "这是评论内容"
+		}
+	});
+});
+
+
+
+/**
+ * 页面路由，从模板渲染页面渲染页面, 
+ * htttp://localhost:8080/user
+ * 支持 ejs, jade 模板
+ */
+app.get('/user', function(req, res) {
+	res.render('user.ejs', {
+		username: '饥人谷'
+	});
+});
