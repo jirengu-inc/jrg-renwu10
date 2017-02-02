53a65ff019ec9189798aac2c441e439e6789d512
code-zhangrui <code.zhangrui@gmail.com>
2017-02-02 20:52:11 +0800

    senior4

---
+++ homework/张睿/senior4/build.js
@@ -0,0 +1,8 @@
+({
+  baseUrl: './src/js',
+  paths: {
+    'jquery': 'lib/jquery.min'
+  },
+  name: 'main',
+  out: 'dist/js/merge.js'
+})
---
+++ homework/张睿/senior4/src/js/com/exposure.js
@@ -0,0 +1,53 @@
+define(['jquery'], function($) {
+
+	function Exposure($node) {
+		this.$node = $node;
+		this.clock = false;
+		this.init();
+	}
+
+	Exposure.prototype = {
+
+		init: function() {
+			this.bind();
+		},
+
+		bind: function() {
+			var that = this;
+			$(window).on('scroll', function() {
+				if(that.clock) {
+					clearTimeout(that.clock);
+				}
+				that.clock = setTimeout(function() {
+					that.checkShow();
+				}, 200);
+			});
+		},
+
+		checkShow: function() {
+			var that = this;
+			this.$node.each(function() {
+				var $cur = $(this);
+				if($cur.data('isLoading')) {
+					return;
+				}
+				if(that.isShow($cur)) {
+					that.exposure($cur);
+				}
+			})
+		},
+
+		isShow: function($node) {
+			var winH = $(window).height(),
+					scrollH = $(window).scrollTop(),
+					top = $node.offset().top;
+			return (top < winH + scrollH) ? true : false;
+		},
+
+		exposure: function($node) {
+			$node.animate({opacity: 1}, 1000);
+		}
+	};
+
+	return Exposure;
+});
