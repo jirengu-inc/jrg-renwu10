278fa810bafac159477a5695393ddb5c68b0ef8a
cui <sure075846@gmail.com>
2017-02-06 14:01:27 +0800

    进阶15

---
+++ homework/崔硕/进阶任务15/router.js
@@ -0,0 +1,10 @@
+app.get('/getMore',function(req,res){
+	var len = req.query.len;
+	var index = req.query.index;
+	var loadMore = index*len;
+	var arr = [loadMore,loadMore+1,loadMore+2];
+	res.send({
+		status: 0,
+		arr
+	})
+});
