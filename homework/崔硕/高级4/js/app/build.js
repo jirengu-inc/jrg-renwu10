14295ffac5f9006f47c95b096d92cc1fe71cb768
cui <sure075846@gmail.com>
2017-02-15 16:04:55 +0800

    高级4

---
+++ homework/崔硕/高级4/js/app/build.js
@@ -0,0 +1,11 @@
+/**
+ * Created by NICK on 2016/10/3.
+ */
+({
+    baseUrl: "../../js",
+    paths: {
+        'jquery': 'com/jquery.min'
+    },
+    name: "app/main",
+    out: "dist/merge.js"
+})
